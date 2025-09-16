87a7b6501ba2e94a4be98223e064520496e07e45
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-09-16 13:12:50 -0300

    docs: files structure

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'models/Product.php';
-
-$product1 = new Product('Celular', 'Celular Samsung', 3500);
-
-echo "{$product1->getName()}\n";
-echo "{$product1->getDescription()}\n";
-echo "{$product1->getPrice()}\n";
